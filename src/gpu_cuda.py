91dce3b6a67f61e7c1581838138537f7f3dc0a66
mecha2k <mecha2k@naver.com>
2020-11-30 11:40:00 +0900

    init

--- src/gpu_cuda.py
+++ src/gpu_cuda.py
@@ -4,3 +4,13 @@ from tensorflow.python.client import device_lib
 sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 
 print(device_lib.list_local_devices())
+
+import sys
+
+print(sys.path)
+
+
+import cv2
+
+print(cv2.__version__)
+print(cv2.cuda.getCudaEnabledDeviceCount())
