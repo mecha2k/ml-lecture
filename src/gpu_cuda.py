4505ff265cc741ab0acd6d97493ea85c8779282d
mecha2k <mecha2k@naver.com>
2021-01-12 12:36:09 +0900

    init

--- src/gpu_cuda.py
+++ src/gpu_cuda.py
@@ -13,3 +13,11 @@ print(cv2.cuda.getCudaEnabledDeviceCount())
 import sys
 
 print(sys.path)
+
+
+gpus = tf.config.experimental.list_physical_devices("GPU")
+if gpus:
+    try:
+        tf.config.experimental.set_visible_devices(gpus[0], "GPU")
+    except RuntimeError as e:
+        print(e)
