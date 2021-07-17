6177fbc3df4875c3784f63c72edc6e0f37ce50ff
mecha2k <mecha2k@naver.com>
2021-07-18 08:07:53 +0900

    yfinance

--- src/python/trading/yahoo-finance.py
+++ src/python/trading/yahoo-finance.py
@@ -1,5 +1,6 @@
 import yfinance as yf
 import pandas as pd
+import datetime
 import os
 
 
@@ -7,6 +8,17 @@ def get_yahoo_data():
     apple = yf.Ticker("AAPL")
     apple = yf.download("AAPL", start="2019-01-01")
     print(apple.info)
+
+    print(datetime.datetime.now())
+
+    start = datetime.datetime(2010, 1, 1)
+    end = datetime.datetime(2011, 1, 1)
+    print(start, end)
+
+    samsung = yf.Ticker("005930.KS")
+    samsung = yf.download("005930.KS", start="2010-01-01", end="2011-01-01")
+    samsung = yf.download("005930.KS", start=start, end=end)
+    print(samsung.info)
 
 
 if __name__ == "__main__":
