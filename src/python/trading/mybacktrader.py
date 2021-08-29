e4d23422a061ddd75d7f90184245d24a24a0e3bb
mecha2k <mecha2k@naver.com>
2021-08-30 06:48:47 +0900

    backtrader

--- src/python/trading/mybacktrader.py
+++ src/python/trading/mybacktrader.py
@@ -24,12 +24,10 @@ class TestStrategy(bt.Strategy):
     def __init__(self):
         # Keep a reference to the "close" line in the data[0] dataseries
         self.dataclose = self.datas[0].close
-
         # To keep track of pending orders and buy price/commission
         self.order = None
         self.buyprice = None
         self.buycomm = None
-
         # Add a MovingAverageSimple indicator
         self.sma = bt.indicators.SimpleMovingAverage(self.datas[0], period=self.params.maperiod)
 
@@ -37,7 +35,6 @@ class TestStrategy(bt.Strategy):
         if order.status in [order.Submitted, order.Accepted]:
             # Buy/Sell order submitted/accepted to/by broker - Nothing to do
             return
-
         # Check if an order has been completed
         # Attention: broker could reject order if not enough cash
         if order.status in [order.Completed]:
@@ -46,7 +43,6 @@ class TestStrategy(bt.Strategy):
                     "BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f"
                     % (order.executed.price, order.executed.value, order.executed.comm)
                 )
-
                 self.buyprice = order.executed.price
                 self.buycomm = order.executed.comm
             else:  # Sell
@@ -54,47 +50,35 @@ class TestStrategy(bt.Strategy):
                     "SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f"
                     % (order.executed.price, order.executed.value, order.executed.comm)
                 )
-
             self.bar_executed = len(self)
-
         elif order.status in [order.Canceled, order.Margin, order.Rejected]:
             self.log("Order Canceled/Margin/Rejected")
-
         # Write down: no pending order
         self.order = None
 
     def notify_trade(self, trade):
         if not trade.isclosed:
             return
-
         self.log("OPERATION PROFIT, GROSS %.2f, NET %.2f" % (trade.pnl, trade.pnlcomm))
 
     def next(self):
         # Simply log the closing price of the series from the reference
         self.log("Close, %.2f" % self.dataclose[0])
-
         # Check if an order is pending ... if yes, we cannot send a 2nd one
         if self.order:
             return
-
         # Check if we are in the market
         if not self.position:
-
             # Not yet ... we MIGHT BUY if ...
             if self.dataclose[0] > self.sma[0]:
-
                 # BUY, BUY, BUY!!! (with all possible default parameters)
                 self.log("BUY CREATE, %.2f" % self.dataclose[0])
-
                 # Keep track of the created order to avoid a 2nd order
                 self.order = self.buy()
-
         else:
-
             if self.dataclose[0] < self.sma[0]:
                 # SELL, SELL, SELL!!! (with all possible default parameters)
                 self.log("SELL CREATE, %.2f" % self.dataclose[0])
-
                 # Keep track of the created order to avoid a 2nd order
                 self.order = self.sell()
 
@@ -114,7 +98,7 @@ if __name__ == "__main__":
     # because it could have been called from anywhere
     modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
     ic(modpath)
-    datapath = os.path.join(os.getcwd(), "src/data/oracle.csv")
+    datapath = os.path.join(os.getcwd(), "data/oracle.csv")
     ic(datapath)
 
     # Create a Data Feed
