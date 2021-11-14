0f31e7177c43801578f83fe58dd2d7a4cc1b146d
mecha2k <mecha2k@naver.com>
2021-11-14 14:43:01 +0900

    datetime

--- src/python/basic/datetime_ex.py
+++ src/python/basic/datetime_ex.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 from datetime import datetime
 
 start = datetime(2018, 1, 1)
@@ -15,9 +16,14 @@ students = [
     ("John", 16, "New York", "US"),
     ("Mike", 17, "las vegas", "US"),
 ]
-df = pd.DataFrame(
-    students, columns=["Name", "Age", "City", "Country"], index=["a", "b", "c", "d", "e", "f"]
-)
+df = pd.DataFrame(students, columns=["Name", "Age", "City", "Country"], index=["a", "b", "c", "d", "e", "f"])
 print(df)
 mod_df = df.append({"Name": "Sahil", "Age": 22}, ignore_index=True)
 print(mod_df)
+
+start_str = start.strftime("%Y%m%d")
+print(start_str)
+
+ticker = "A005930"
+ticker = re.findall(r"\d+", ticker)
+print(ticker[0])
--- src/python/package/main.py
+++ src/python/package/main.py
@@ -3,3 +3,4 @@ from calcpkg import *
 if __name__ == "__main__":
     print(dir())
     print(operation.add(2, 3))
+    print(ord("1"))
