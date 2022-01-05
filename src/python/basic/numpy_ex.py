4608d479072aaa8836fb84b2ad0d0651d0f64cdc
mecha2k <mecha2k@naver.com>
2022-01-05 14:22:52 +0900

    numpy

--- src/python/basic/numpy_ex.py
+++ src/python/basic/numpy_ex.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 ax = np.arange(0, 20, 2)
 ay = np.array([0, 1, 2, 2, 0, 1, 1, 1, 2, 0])
@@ -24,3 +25,11 @@ time_steps = np.broadcast_to(time_step, (n_sims, N))
 print(time_step)
 print(time_steps)
 print(time_steps.shape)
+
+s = np.random.dirichlet((10, 5, 1), 20).T
+print(s)
+plt.bar(range(20), s[0])
+plt.bar(range(20), s[1], bottom=s[0], color="g")
+plt.bar(range(20), s[2], bottom=s[0] + s[1], color="r")
+plt.title("Lengths of Strings")
+plt.show()
