767655392c8f3b85d595ff7d463efd0b9edec171
mecha2k <mecha2k@naver.com>
2022-04-18 15:40:10 +0900

    torch

--- src/deeplearning/torch_full.py
+++ src/deeplearning/torch_full.py
@@ -4,5 +4,12 @@ print(torch.__version__)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(f"{device} is available in torch")
 
-if __name__ == '__main__':
-    pass
+
+def torch_variables():
+    x = torch.zeros(size=(4, 2), dtype=torch.int32)
+    x = torch.tensor(data=[3.0, 2.4])
+    print(x)
+
+
+if __name__ == "__main__":
+    torch_variables()
