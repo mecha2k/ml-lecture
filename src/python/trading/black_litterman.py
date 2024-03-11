716dd32a2e678a7697e36a2b0596609964e3c8d4
mecha2k <mecha2k@naver.com>
2024-03-11 11:41:05 +0900

    black_litterman.py

--- src/python/trading/black_litterman.py
+++ src/python/trading/black_litterman.py
@@ -27,48 +27,77 @@ startDate = datetime(2018, 1, 1)
 endDate = datetime(2019, 12, 31)
 
 
+# 무위험수익률, 수익률,공분산으로 효율적 프런티어 계산
+def solveFrontier(rets, covs, rf):
+    # 최적비중계산 목적함수
+    def obj_func(weights, rets, covs, rf):
+        mean = sum(rets * weights)
+        var = np.dot(np.dot(weights, covs), weights)
+        # 최적화 제약조건 페널티
+        penalty = 100 * abs(mean - rf)
+        return var + penalty
+
+    # 효율적 프론티어를 구성하는 평균-분산을 돌려줄 리스트 준비
+    frontier_mean, frontier_var = [], []
+    n_rets = len(rets)  # 투자자산 갯수
+    # 수익률 최저~최대 사이를 반복
+    for ret in np.linspace(min(rets), max(rets), num=20):
+        # 최적화 함수에 전달할 초기값으로 동일비중으로 시작
+        weights = np.ones([n_rets]) / n_rets
+        # 최적화 함수에 전달할 범위조건과 제약조건을 미리 준비
+        # 범위조건: 각 구성자산의 투자비중은 0~100% 사이
+        # 제약조건: 전체 투자비중은 100%
+        bnds = [(0, 1) for i in range(n_rets)]
+        cons = ({'type': 'eq', 'fun': lambda wgt: sum(wgt) - 1})
+        # 최적화 함수 minimize()은 최적화할 obj함수와 최적화를 시작할 초깃값을 인수로 받음
+        results = minimize(obj_func, weights, (rets, covs, ret), method='SLSQP', constraints=cons, bounds=bnds)
+        if not results.success:
+            raise BaseException(results.message)
+        # 효율적 프런티어 평균과 분산리스트에
+        # 최적포트폴리오 수익률과 분산 추가
+        frontier_mean.append(ret)
+        frontier_var.append(np.dot(np.dot(results.x, covs), results.x))
+
+    return np.array(frontier_mean), np.array(frontier_var)
+
 # 무위험수익률, 수익률,공분산으로 샤프비율을 최대로 하는 접점포트폴리오 비중 계산
-def solveWeights(R, C, rf):
-    # 파이썬은 함수안에 함수를 정의할 수 있다
-    # 최적비중계산을 위해 다음과 같이 목적함수를 정의한다
-    def obj(W, R, C, rf):
-        mean = sum(R * W)
-        var = np.dot(np.dot(W, C), W)
-        # 샤프비율을 효용함수로 한다
+def solveWeights(rets_annual, covs_annual, rf=0.015):
+    # 최적비중계산 목적함수
+    def obj_func(weights, rets, covs, rf):
+        mean = sum(rets * weights)
+        var = np.dot(np.dot(weights, covs), weights)
+        # 효용함수 : 샤프비율
         util = (mean - rf) / np.sqrt(var)
-        # 효용함수 극대화= 효용함수 역함수를 최소화하는 것이다.
+        # 효용함수 극대화 = 효용함수 역함수를 최소화
         return 1 / util
 
-    n = len(R)  # 투자자산 갯수
+    n_rets = len(rets_annual)  # 투자자산 갯수
 
     # 동일비중으로 최적화 시작
-    W = np.ones([n]) / n
-    # 비중범위는 0~100%사이(공매도나 차입조건이 없음)
-    bnds = [(0., 1.) for i in range(n)]
+    weights = np.ones([n_rets]) / n_rets
+    # 비중범위는 0~100%사이 (공매도나 차입조건이 없음)
+    bnds = [(0, 1) for i in range(n_rets)]
     # 제약조건은 비중합=100%
-    cons = ({'type': 'eq', 'fun': lambda W: sum(W) - 1.})
+    cons = ({'type': 'eq', 'fun': lambda wgt: sum(wgt) - 1})
     # 최적화
-    res = minimize(obj, W, (R, C, rf), method='SLSQP', constraints=cons, bounds=bnds)
-    if not res.success:
-        # 최적화 실패한 경우
-        raise BaseException(res.message)
-    # 최적화 결과를 돌려준다
-    return res.x
+    results = minimize(obj_func, weights, (rets_annual, covs_annual, rf), method='SLSQP', constraints=cons, bounds=bnds)
+    if not results.success:
+        raise BaseException(results.message)
+
+    return results.x
 
 # 효율적 포트폴리오 최적화
 def optimizeFrontier(rets_annual, covs_annual, rf=0.015):
     # 접점포트폴리오 계산
     weights = solveWeights(rets_annual, covs_annual, rf)
     # 투자비중으로 계산한 평균과 분산
-    tan_mean = sum(R * W)
-    tan_var = np.dot(np.dot(W, C), W)
-
+    tan_mean = sum(rets_annual * weights)
+    tan_var = np.dot(np.dot(rets_annual, covs_annual), weights)
     # 효율적 포트폴리오 계산
-    eff_mean, eff_var = solveFrontier(R, C, rf)
+    eff_mean, eff_var = solveFrontier(rets_annual, covs_annual, rf)
 
-    # 비중, 접점포트폴리오의 평균/분산, 효율적 포트폴리오의 평균/분산을
-    # 딕셔너리 데이터형으로 돌려준다
-    return {'weights':W, 'tan_mean':tan_mean, 'tan_var':tan_var, 'eff_mean':eff_mean, 'eff_var':eff_var}
+    # 비중, 접점포트폴리오의 평균/분산, 효율적 포트폴리오의 평균/분산
+    return {'weights':weights, 'tan_mean':tan_mean, 'tan_var':tan_var, 'eff_mean':eff_mean, 'eff_var':eff_var}
 
 if __name__ == "__main__":
     filepath = "../../data/stock_data.pickle"
@@ -122,6 +151,7 @@ if __name__ == "__main__":
     print(pd.DataFrame(covs_annual, columns=tickers, index=tickers))
 
     # 과거 데이터를 이용한 최적화
-    # opt1 = optimize_frontier(R, C, rf)
+    optim1 = optimizeFrontier(rets_annual, covs_annual, rf)
+    print(optim1)
 
 
