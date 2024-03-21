1003ef45dae8536e613bb53f57a6f972cf5685cc
mecha2k <mecha2k@naver.com>
2024-03-21 17:28:50 +0900

    pytorch_lightning

--- src/pytorch/lightning_basic.py
+++ src/pytorch/lightning_basic.py
@@ -16,7 +16,9 @@ warnings.filterwarnings("ignore", category=UserWarning)
 class Encoder(nn.Module):
     def __init__(self):
         super().__init__()
-        self.l1 = nn.Sequential(nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 3))
+        self.l1 = nn.Sequential(
+            nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 3)
+        )
 
     def forward(self, x):
         return self.l1(x)
@@ -25,7 +27,9 @@ class Encoder(nn.Module):
 class Decoder(nn.Module):
     def __init__(self):
         super().__init__()
-        self.l1 = nn.Sequential(nn.Linear(3, 64), nn.ReLU(), nn.Linear(64, 28 * 28))
+        self.l1 = nn.Sequential(
+            nn.Linear(3, 64), nn.ReLU(), nn.Linear(64, 28 * 28)
+        )
 
     def forward(self, x):
         return self.l1(x)
